d7e03c30cf2d07e2de0e68b23ce97a37a132e79a
Hizkia Felix Winata <felix@hizkifw.me>
2021-03-12 18:17:01 +0800

    feat: publish by HTTP POST

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const http = require("http");
 const WebSocket = require("ws");
 const port = process.env.PORT || 42069;
-const server = new WebSocket.Server({ port });
+
+let sockets = [];
+const subscriptions = {};
 
 const ERRORS = {
   METHOD_NOT_FOUND: {
@@ -9,45 +12,80 @@ const ERRORS = {
   },
 };
 
-let sockets = [];
-const subscriptions = {};
+const handleMessage = ({ method, params, ...rest }, socket) => {
+  id = rest.id || 0;
 
-server.on("connection", (socket) => {
+  if (method === "publish") {
+    const [subId, data] = params;
+    if (Array.isArray(subscriptions[subId])) {
+      for (const sub of subscriptions[subId]) {
+        try {
+          sub.send(JSON.stringify({ id: -1, result: [subId, data] }));
+        } catch (ex) {}
+      }
+    }
+    return { id, result: "Success" };
+  } else if (method === "subscribe") {
+    for (const subId of params) {
+      if (!Array.isArray(subscriptions[subId])) subscriptions[subId] = [];
+      subscriptions[subId].push(socket);
+    }
+    return { id, result: "Success" };
+  } else if (method === "unsubscribe") {
+    for (const subId of params) {
+      if (!Array.isArray(subscriptions[subId])) subscriptions[subId] = [];
+      subscriptions[subId] = subscriptions[subId].filter((s) => s !== socket);
+    }
+    return { id, result: "Success" };
+  }
+
+  return { id, error: ERRORS.METHOD_NOT_FOUND };
+};
+
+const readJSON = async (stream) =>
+  new Promise((resolve, reject) => {
+    let stringContent = "";
+    stream.on("data", (chunk) => {
+      stringContent += chunk.toString();
+    });
+    stream.on("end", () => {
+      try {
+        resolve(JSON.parse(stringContent));
+      } catch (ex) {
+        reject(ex);
+      }
+    });
+  });
+
+const server = http.createServer((req, res) => {
+  try {
+    if (req.method !== "POST") {
+      res.writeHead(405, { Allow: "POST" });
+      res.write("Method not allowed");
+      res.end();
+      return;
+    }
+    readJSON(req).then((json) => {
+      const handleResult = JSON.stringify(handleMessage(json, null));
+      res.writeHead(200);
+      res.write(handleResult);
+      res.end();
+    });
+  } catch (err) {
+    res.writeHead(400);
+    res.write(err.toString());
+    res.end();
+  }
+});
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (socket) => {
   sockets.push(socket);
 
   socket.on("message", (msg) => {
     let id = 0;
     try {
-      const { method, params, ...rest } = JSON.parse(msg);
-      id = rest.id || 0;
-
-      if (method === "subscribe") {
-        for (const subId of params) {
-          if (!Array.isArray(subscriptions[subId])) subscriptions[subId] = [];
-          subscriptions[subId].push(socket);
-        }
-        socket.send(JSON.stringify({ id, result: "Success" }));
-      } else if (method === "unsubscribe") {
-        for (const subId of params) {
-          if (!Array.isArray(subscriptions[subId])) subscriptions[subId] = [];
-          subscriptions[subId] = subscriptions[subId].filter(
-            (s) => s !== socket
-          );
-        }
-        socket.send(JSON.stringify({ id, result: "Success" }));
-      } else if (method === "publish") {
-        const [subId, data] = params;
-        if (Array.isArray(subscriptions[subId])) {
-          for (const sub of subscriptions[subId]) {
-            try {
-              sub.send(JSON.stringify({ id: -1, result: [subId, data] }));
-            } catch (ex) {}
-          }
-        }
-        socket.send(JSON.stringify({ id, result: "Success" }));
-      } else {
-        socket.send(JSON.stringify({ id, error: ERRORS.METHOD_NOT_FOUND }));
-      }
+      socket.send(JSON.stringify(handleMessage(JSON.parse(msg), socket)));
     } catch (ex) {
       socket.send(
         JSON.stringify({ id, error: { message: ex.message, code: -1 } })
@@ -64,6 +102,8 @@ server.on("connection", (socket) => {
   });
 });
 
-server.on("listening", () => {
+wss.on("listening", () => {
   console.log(`Listening on port ${port}`);
 });
+
+server.listen(port);
